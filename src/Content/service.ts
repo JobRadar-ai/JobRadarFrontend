a57be24c481f6a10dd63545825685d4091b0eca6
rituraj4857 <ritu.12797.raj@gmail.com>
2024-04-06 20:24:20 +0530

    addec content for pages

---
+++ src/Content/service.ts
@@ -0,0 +1,17 @@
+export const service  = [
+    {
+        icon:2,
+        head:"Campaign Management",
+        desc:" This is yet to come, allowing user to create dedicated mailing records"
+    },
+    {
+        icon:1,
+        head:"Cold mailing",
+        desc:"Establish a presence in the corporate to get  opportunities."
+    },
+    {
+        icon:0,
+        head:"Email Marketing",
+        desc:"Enhance your advertisement by reaching out to a diverse audience "
+    }
+]
